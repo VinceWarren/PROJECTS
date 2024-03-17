4ab871f238c009479ac559df2dce0e43ee8b625f
Vince Pradas <pradasvince@gmail.com>
2024-03-17 23:36:56 +0800

    work

---
+++ server.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const BLOG = require('./models/blog');
+const bodyParser = require('body-parser');
+
+// Connect to MongoDB
+mongoose.set('strictQuery', false);
+const connectDB = async() => {
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
+  }catch(err){
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+connectDB()
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
+// Routes
+app.post('/add-blog', async (req, res) => {
+  try {
+    const { title, author, description } = req.body;
+    const save = new BLOG({
+      title: title,
+      author: author,
+      description: description,
+    });
+    console.log('Data received:', req.body);
+    await save.save();
+    res.status(201).send('Blog post created successfully');
+  } catch (err) {
+    console.error('Error adding blog post:', err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await BLOG.find();
+    const blogs = data.map(blog => ({
+      title: blog.title,
+      author: blog.author,
+      description: blog.description,
+      time: blog.createdAt
+    }));
+    res.render('index', { title: 'Home', blogs });
+  } catch (err) {
+    console.error('Error fetching blogs:', err);
+    res.status(500).send('Internal server error');
+    res.send(morgan('dev'))
+  }
+});
+
+app.get('/about', async (req, res) => {
+  try{
+    res.render('about', { title: 'About' });
+  }
+  catch(err){
+    console.log(err, 'Internal error')
+  }
+});
+
+app.get('/create', (req, res) => {
+  res.render('create', { title: 'New Blog' });
+});
+
+app.use((req, res) => {
+  res.render('404', { title: '404' });
+});
+
+module.exports = app;
